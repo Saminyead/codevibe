b7ec0c20a179ce0117486a064761dc2ea48ecac1
saminyead <Saminyead@gmail.com>
2025-07-04 11:03:15 +0600

    yt searching function added

--- code_vibing/utils.py
+++ code_vibing/utils.py
@@ -2,6 +2,8 @@ from pytubefix import YouTube
 import os
 from datetime import datetime
 
+import requests
+
 from pathlib import Path
 
 def download_track(url:str, dest:str):
@@ -16,3 +18,9 @@ def find_latest_playlist(playlist_path:str|Path, dt_format:str):
     playlists = os.listdir(playlist_path)
     pl_dt_dict = {p:datetime.strptime(p,dt_format) for p in playlists}
     return max(pl_dt_dict)
+
+def search_song_yt(query:str, api_key:str):
+    url = "https://www.googleapis.com/youtube/v3/playlists"
+    params = {"key":api_key, "part":"snippet", "q":query}
+    res = requests.get(url=url, params=params)
+    return res.json()['items'][0]['id']['videoId']
